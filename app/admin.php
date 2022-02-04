0e3fe6aaae2736e4a55cbe92c3e821fc6603a1d9
kawada68 <nodoguro68@gmail.com>
2022-02-04 16:51:51 +0900

    仮管理画面作成

---
+++ app/admin.php
@@ -0,0 +1,24 @@
+<?php
+
+$page_title = '管理画面';
+include '../template/head.php';
+include '../template/header.php';
+?>
+
+<main class="main">
+    <div class="container">
+
+        <section class="section">
+            <h2 class="title">管理画面</h2>
+
+            <div class="link-wrap">
+                <a href="category.php" class="link">カテゴリー</a>
+            </div>
+        </section>
+
+    </div>
+
+</main>
+<?php include '../template/footer.php' ?>
+</body>
+</html>
