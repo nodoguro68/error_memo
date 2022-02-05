c984a7609bcd5c1529e1da7e6c22dc456015f3a7
kawada68 <nodoguro68@gmail.com>
2022-02-05 14:57:50 +0900

    sassコンパイルと同時にcss圧縮

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+var cleancss = require('gulp-clean-css');
 
 // sassコンパイル
 gulp.task('sass', function() {
     return gulp.src('resource/scss/style.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('public/css/'));
+        .pipe(sass())
+        .pipe(cleancss())
+        .pipe(gulp.dest('public/css/'));
 });
 
 // js圧縮
