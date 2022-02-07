004ac1becd93d1d431a71dc6b797d50972dad012
kawada68 <nodoguro68@gmail.com>
2022-02-07 21:34:55 +0900

    headerのスタイル修正

--- template/header.php
+++ template/header.php
@@ -1,15 +1,15 @@
 <header class="header">
     <div class="header-wrap">
         <div class="logo-wrap">
-            <h1 class="logo header__logo"><a href="index.php" class="logo__link">error memo</a></h1>
+            <h1 class="header__logo"><a href="index.php">error memo</a></h1>
         </div>
-        <nav class="nav header__nav">
-            <ul class="nav__menu">
-                <li class="nav__item"><a href="index.php" class="nav__link">HOME</a></li>
-                <li class="nav__item"><a href="signup.php" class="nav__link">ユーザー登録</a></li>
-                <li class="nav__item"><a href="login.php" class="nav__link">ログイン</a></li>
-                <!-- <li class="nav__item"><a href="logout.php" class="nav__link">ログアウト</a></li>
-                <li class="nav__item"><a href="mypage.php" class="nav__link">マイページ</a></li> -->
+        <nav class="header__nav">
+            <ul class="header__menu">
+                <li class="header__item"><a href="index.php" class="header__link">HOME</a></li>
+                <li class="header__item"><a href="signup.php" class="header__link">ユーザー登録</a></li>
+                <li class="header__item"><a href="login.php" class="header__link">ログイン</a></li>
+                <!-- <li class="header__item"><a href="logout.php" class="header__link">ログアウト</a></li>
+                <li class="header__item"><a href="mypage.php" class="header__link">マイページ</a></li> -->
             </ul>
         </nav>
     </div>
