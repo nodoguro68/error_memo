e53efd891272efce65c673737487fa46145e88fb
kawada68 <nodoguro68@gmail.com>
2022-02-07 19:29:21 +0900

    ヘッダーのスタイル作成

--- template/header.php
+++ template/header.php
@@ -1,13 +1,15 @@
-<header class="">
-    <div class="">
-        <h1 class=""><a href="index.php" class="">error memo</a></h1>
-        <nav class="">
-            <ul class="">
-                <li class=""><a href="index.php" class="">HOME</a></li>
-                <li class=""><a href="signup.php" class="">ユーザー登録</a></li>
-                <li class=""><a href="login.php" class="">ログイン</a></li>
-                <li class=""><a href="logout.php" class="">ログアウト</a></li>
-                <li class=""><a href="mypage.php" class="">マイページ</a></li>
+<header class="header">
+    <div class="header-wrap">
+        <div class="logo-wrap">
+            <h1 class="logo header__logo"><a href="index.php" class="logo__link">error memo</a></h1>
+        </div>
+        <nav class="nav header__nav">
+            <ul class="nav__menu">
+                <li class="nav__item"><a href="index.php" class="nav__link">HOME</a></li>
+                <li class="nav__item"><a href="signup.php" class="nav__link">ユーザー登録</a></li>
+                <li class="nav__item"><a href="login.php" class="nav__link">ログイン</a></li>
+                <!-- <li class="nav__item"><a href="logout.php" class="nav__link">ログアウト</a></li>
+                <li class="nav__item"><a href="mypage.php" class="nav__link">マイページ</a></li> -->
             </ul>
         </nav>
     </div>
