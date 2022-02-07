b7060898a6c0b10bb78abd6a6aa5deb8c82da1d3
kawada68 <nodoguro68@gmail.com>
2022-02-07 21:35:06 +0900

    footerのスタイル作成

--- template/footer.php
+++ template/footer.php
@@ -1,12 +1,12 @@
-<footer class="">
-    <div class="">
-        <p class="">Copyright<a href=""></a>.All Rights Reserved</p>
-        <nav class="">
-            <ul class="">
-                <li class=""><a href="" class="">利用規約</a></li>
-                <li class=""><a href="" class="">プライバシーポリシー</a></li>
-                <li class=""><a href="" class="">特定商取引法に基づく表記</a></li>
-                <li class=""><a href="" class="">お問い合わせ</a></li>
+<footer class="footer">
+    <div class="footer-wrap">
+        <small class="footer__copy">&copy; <a href="index.php" class="footer__link">error memo</a></small>
+        <nav class="footer__nav">
+            <ul class="footer__menu">
+                <li class="footer__item"><a href="" class="footer__link">利用規約</a></li>
+                <li class="footer__item"><a href="" class="footer__link">プライバシーポリシー</a></li>
+                <li class="footer__item"><a href="" class="footer__link">特定商取引法に基づく表記</a></li>
+                <li class="footer__item"><a href="" class="footer__link">お問い合わせ</a></li>
             </ul>
         </nav>
     </div>
