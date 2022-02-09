62fd552502a60f1b85bea51306dbcca84c3908c2
kawada68 <nodoguro68@gmail.com>
2022-02-10 02:46:14 +0900

    マイページスタイル作成

--- app/mypage.php
+++ app/mypage.php
@@ -6,57 +6,62 @@ include '../template/header.php';
 ?>
 
 <main class="main">
+    <div class="tab">
+        <ul class="tab__inner">
+            <li class="tab__item">フォルダ</li>
+            <li class="tab__item tab__item--active">未解決</li>
+            <li class="tab__item">解決済</li>
+            <li class="tab__item">いいね</li>
+            <li class="tab__item">設定</li>
+        </ul>
+    </div>
 
     <div class="container">
 
-        <ul class="tab">
-            <li class="tab__item">フォルダ</li>
-            <li class="tab__item">未解決</li>
-            <li class="tab__item">解決済み</li>
-            <li class="tab__item">お気に入り</li>
-            <li class="tab__item">設定</li>
-        </ul>
-
         <div class="tab-menu">
-            <!-- 設定 -->
-            <div class="user-info">
-                <img src="" alt="プロフィール画像" class="user-info__img">
-                <p class="user-info__name">@ユーザーネーム</p>
-                <p>登録日:<span class="user-info__regisration-date">2022/01/01</span></p>
-                <div class="user-info__follow">
-                    <a href="follow.php" class="user-info__link">フォロー</a>
-                    <a href="follow.php" class="user-info__link">フォロワー</a>
-                </div>
-                <div class="user-info__memo-count">50件</div>
-            </div>
-        </div>
-
-        <ul class="mypage-nav">
-            <li class="mypage-nav__item"><a href="profile.php" class="mypage-nav__link">プロフィール編集</a></li>
-            <li class="mypage-nav__item"><a href="edit_pass.php" class="mypage-nav__link">パスワード変更</a></li>
-            <li class="mypage-nav__item"><a href="signout.php" class="mypage-nav__link">退会</a></li>
-        </ul>
-
-        <!-- フォルダ -->
-        <div class="tab-menu">
-            <ul class="list">
-                <li class="list__item"><a href="mypage.php" class="list__link">フォルダ<span class="list__delete-btn"></span></a></li>
-            </ul>
-
-            <form method="post" class="form">
-                <input type="text" name="create_folder" class="form__input" placeholder="フォルダを作成">
-                <input type="submit" value="＋" class="form__submit-btn" disabled>
+            <form method="post" class="folder-form">
+                <input type="text" name="create_folder" class="folder-form__input" placeholder="フォルダを作成">
+                <input type="submit" value="＋" class="folder-form__submit-btn" disabled>
                 <span class="err-msg"></span>
             </form>
+
+            <ul class="folder-list">
+                <li class="folder-list__item">
+                    <a href="folder.php?folder_id=" class="folder-list__link">
+                        フォルダ
+                        <span class="folder-list__count">1</span>
+                    </a>
+                </li>
+                <li class="folder-list__item">
+                    <a href="folder.php?folder_id=" class="folder-list__link">
+                        フォルダ
+                        <span class="folder-list__count">0</span>
+                    </a>
+                </li>
+                <li class="folder-list__item">
+                    <a href="folder.php?folder_id=" class="folder-list__link">
+                        フォルダ
+                        <span class="folder-list__count">17</span>
+                    </a>
+                </li>
+                <li class="folder-list__item">
+                    <a href="folder.php?folder_id=" class="folder-list__link">
+                        フォルダ
+                        <span class="folder-list__count">100</span>
+                    </a>
+                </li>
+            </ul>
         </div>
 
         <div class="tab-menu">
-            <!-- 未解決 -->
             <ul class="memo-list">
                 <li class="memo-list__item">
                     <a href="memo_form.php" class="memo-list__link">
-                        未解決メモ
-                        <span class="memo-list__date">2022/01/01</span>
+                        <div class="memo-list__head">
+                            <span class="memo-list__folder">フォルダ</span>
+                            <span class="memo-list__date">2022/01/01</span>
+                        </div>
+                        <span class="memo-list__title">未解決メモ</span>
                     </a>
                 </li>
             </ul>
@@ -64,27 +69,97 @@ include '../template/header.php';
 
 
         <div class="tab-menu">
-            <!-- 解決済み -->
             <ul class="memo-list">
                 <li class="memo-list__item">
-                    <a href="memo_form.php" class="memo-list__link">
-                        解決済みメモ
-                        <span class="memo-list__date">2022/01/01</span>
+                    <a href="" class="memo-list__link">
+                        <div class="memo-list__head">
+                            <span class="memo-list__folder">フォルダ</span>
+                            <span class="memo-list__date">2022/01/01</span>
+                        </div>
+                        <span class="memo-list__title">解決済メモ</span>
                     </a>
                 </li>
             </ul>
         </div>
 
         <div class="tab-menu">
-            <!-- お気に入り -->
             <ul class="memo-list">
                 <li class="memo-list__item">
-                    <a href="memo_detail.php" class="memo-list__link">
-                        お気に入りメモ
-                        <span class="memo-list__date">2022/01/01</span>
+                    <a href="" class="memo-list__link">
+                        <div class="memo-list__head">
+                            <span class="memo-list__username">@ユーザー</span>
+                            <span class="memo-list__date">2022/01/01</span>
+                        </div>
+                        <span class="memo-list__title">エラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラーエラー</span>
                     </a>
                 </li>
             </ul>
+        </div>
+
+        <div class="tab-menu tab-menu--active">
+            <div class="mypage-contents">
+                <div class="user-info">
+                    <div class="img-wrap">
+                        <img src="../public/images/no-image.jpeg" alt="プロフィール画像" class="user-info__img">
+                    </div>
+                    <p class="user-info__name">@ユーザーネーム</p>
+                    <p>登録日:<span class="user-info__regisration-date">2022/01/01</span></p>
+                    <div class="user-info__follow">
+                        <a href="follow.php" class="user-info__link">0フォロー</a>
+                        <a href="follow.php" class="user-info__link">0フォロワー</a>
+                    </div>
+                    <div class="user-info__count-area">
+                        <div class="user-info__count-item">
+                            未解決<span class="user-info__count">50</span>件
+                        </div>
+                        <div class="user-info__count-item">
+                            解決済<span class="user-info__count">50</span>件
+                        </div>
+                        <div class="user-info__count-item">
+                            全<span class="user-info__count">50</span>件
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mypage-contents">
+                <ul class="url-list">
+                    <li class="url-list__item">
+                        <span class="url-list__title">Github</span>
+                        <a href="" class="url-list__link">Github</a>
+                    </li>
+                    <li class="url-list__item">
+                        <span class="url-list__title">Facebook</span>
+                        <a href="" class="url-list__link">Facebook</a>
+                    </li>
+                    <li class="url-list__item">
+                        <span class="url-list__title">Twitter</span>
+                        <a href="" class="url-list__link">Twitter</a>
+                    </li>
+                    <li class="url-list__item">
+                        <span class="url-list__title">Qitta</span>
+                        <a href="" class="url-list__link">Qitta</a>
+                    </li>
+                    <li class="url-list__item">
+                        <span class="url-list__title">Blog</span>
+                        <a href="" class="url-list__link">Blog</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mypage-contents">
+                <ul class="mypage-menu">
+                    <li class="mypage-menu__item">
+                        <a href="profile.php" class="mypage-menu__link">プロフィール編集</a>
+                    </li>
+                    <li class="mypage-menu__item">
+                        <a href="pass_edit.php" class="mypage-menu__link">パスワード変更</a>
+                    </li>
+                    <li class="mypage-menu__item">
+                        <a href="signout.php" class="mypage-menu__link">退会</a>
+                    </li>
+                </ul>
+            </div>
         </div>
 
     </div>
