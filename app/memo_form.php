fcd0b908f57ff85cfe80fcc553a59ed2ac61b517
kawada68 <nodoguro68@gmail.com>
2022-02-09 19:13:34 +0900

    メモフォームのスタイル作成

--- app/memo_form.php
+++ app/memo_form.php
@@ -9,82 +9,104 @@ include '../template/header.php';
 <main class="main">
     <div class="container">
 
-        <a href="mypage.php" class="">＜ フォルダ</a>
+        <div class="link-wrap">
+            <a href="" class="link">＞ 戻る</a>
+        </div>
 
         <form method="post" class="memo-form">
-            <div class="form__header">
-                <h2 class="form__title"><?= ($editFlag) ? 'メモ編集' : 'メモ新規登録'; ?></h2>
-                <div class="btn-wrap">
-                    <button type="submit" name="delete_memo" value="" class="">削除</button>
-                </div>
-                <span class="err-msg"></span>
-            </div>
+            <div class="memo-form__inner">
+                <div class="memo-form__header">
+                    <h2 class="memo-form__title"><?= ($editFlag) ? 'メモ編集' : 'メモ新規登録'; ?></h2>
+                    <div class="err-msg__area">
+                        <span class="err-msg"></span>
+                    </div>
 
-            <div class="form__body">
-                <!-- エラータイトル -->
-                <div class="form__item">
-                    <label for="title" class="form__label">エラータイトル</label>
-                    <input type="text" name="title" class="form__input" id="title" value="" autofocus>
-                    <span class="err-msg"></span>
+                    <div class="memo-form__item--2column">
+                        <!-- カテゴリー -->
+                        <div class="memo-form__selectbox-wrap">
+                            <select name="category_id" id="category" class="memo-form__selectbox">
+                                <option value="0">未分類</option>
+                                <option value="0">PHP</option>
+                                <option value="0">PHP</option>
+                            </select>
+                            <span class="err-msg"></span>
+                        </div>
+                        <div class="memo-form__radio-group">
+                            <!-- 公開するかどうか -->
+                            <div class="memo-form__radio">
+                                <label for="private" class="memo-form__label">非公開</label><input type="radio" name="is_published" value="0" class="memo-form__radio-btn" id="private">
+                                <label for="public" class="memo-form__label">公開</label><input type="radio" name="is_published" value="1" class="memo-form__radio-btn" id="public">
+                            </div>
+                            <!-- 解決済みかどうか -->
+                            <div class="memo-form__radio">
+                                <label for="unsolved" class="memo-form__label">未解決</label><input type="radio" name="is_solved" value="0" class="memo-form__radio-btn" id="unsolved">
+                                <label for="solved" class="memo-form__label">解決済み</label><input type="radio" name="is_solved" value="1" class="memo-form__radio-btn" id="solved">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- エラータイトル -->
+                    <div class="memo-form__item">
+                        <label for="title" class="memo-form__label">エラータイトル</label>
+                        <span class="memo-form__require-label">必須</span>
+                        <input type="text" name="title" class="memo-form__input" id="title" value="" autofocus>
+                        <span class="err-msg"></span>
+                    </div>
                 </div>
-                <!-- 公開するかどうか -->
-                <div class="form__item">
-                    <label for="private" class="form__label">非公開</label><input type="radio" name="is_published" value="0" class="form__radio-btn" id="private">
-                    <label for="public" class="form__label">公開</label><input type="radio" name="is_published" value="1" class="form__radio-btn" id="public">
-                    <span class="err-msg"></span>
+
+                <div class="memo-form__body">
+                    <!-- エラー内容 -->
+                    <div class="memo-form__item">
+                        <label for="content" class="memo-form__label">エラー内容</label>
+                        <textarea name="content" id="content" class="memo-form__textarea"></textarea>
+                        <span class="err-msg"></span>
+                    </div>
+                    <!-- やりたいこと -->
+                    <div class="memo-form__item">
+                        <label for="ideal" class="memo-form__label">やりたいこと</label>
+                        <textarea name="ideal" id="ideal" class="memo-form__textarea"></textarea>
+                        <span class="err-msg"></span>
+                    </div>
+                    <!-- 試したこと -->
+                    <div class="memo-form__item">
+                        <label for="attempt" class="memo-form__label">試したこと</label>
+                        <textarea name="attempt[]" class="memo-form__textarea memo-form__textarea--dynamic"></textarea>
+                        <span class="err-msg"></span>
+                        <div class="btn-wrap">
+                            <button type="button" class="add-input-btn">＋</button>
+                        </div>
+                    </div>
+                    <!-- 解決方法 -->
+                    <div class="memo-form__item">
+                        <label for="solution" class="memo-form__label">解決方法</label>
+                        <textarea name="solution" id="solution" class="memo-form__textarea"></textarea>
+                        <span class="err-msg"></span>
+                    </div>
+                    <!-- 原因 -->
+                    <div class="memo-form__item">
+                        <label for="cause" class="memo-form__label">原因</label>
+                        <textarea name="cause" id="cause" class="memo-form__textarea"></textarea>
+                        <span class="err-msg"></span>
+                    </div>
+                    <!-- 参考 -->
+                    <div class="memo-form__item">
+                        <label for="reference" class="memo-form__label">参考</label>
+                        <input type="text" name="reference[]" class="memo-form__input"  value="">
+                        <input type="text" name="reference" class="memo-form__input"  value="">
+                        <span class="err-msg"></span>
+                        <div class="btn-wrap">
+                            <button type="button" class="add-input-btn">＋</button>
+                        </div>
+                    </div>
+                    <!-- その他 -->
+                    <div class="memo-form__item">
+                        <label for="etc" class="memo-form__label">その他</label>
+                        <textarea name="etc" id="etc" class="memo-form__textarea"></textarea>
+                        <span class="err-msg"></span>
+                    </div>
                 </div>
-                <!-- 解決済みかどうか -->
-                <div class="form__item">
-                    <label for="unsolved" class="form__label">未解決</label><input type="radio" name="is_solved" value="0" class="form__radio-btn" id="unsolved">
-                    <label for="solved" class="form__label">解決済み</label><input type="radio" name="is_solved" value="1" class="form__radio-btn" id="solved">
-                    <span class="err-msg"></span>
-                </div>
-                <!-- カテゴリー -->
-                <div class="form__item">
-                    <label for="category" class="form__label">カテゴリー</label>
-                    <select name="category_id" id="category" class="form__selectbox">
-                        <option value="0"></option>
-                    </select>
-                    <span class="err-msg"></span>
-                </div>
-                <!-- やりたいこと -->
-                <div class="form__item">
-                    <label for="ideal" class="form__label">やりたいこと</label>
-                    <textarea name="ideal" id="ideal" class="form__textarea"></textarea>
-                    <span class="err-msg"></span>
-                </div>
-                <!-- 試したこと -->
-                <div class="form__item">
-                    <label for="attempt" class="form__label">試したこと</label>
-                    <textarea name="attempt" id="attempt" class="form__textarea"></textarea>
-                    <span class="err-msg"></span>
-                </div>
-                <!-- 解決方法 -->
-                <div class="form__item">
-                    <label for="solution" class="form__label">解決方法</label>
-                    <textarea name="solution" id="solution" class="form__textarea"></textarea>
-                    <span class="err-msg"></span>
-                </div>
-                <!-- 原因 -->
-                <div class="form__item">
-                    <label for="cause" class="form__label">原因</label>
-                    <textarea name="cause" id="cause" class="form__textarea"></textarea>
-                    <span class="err-msg"></span>
-                </div>
-                <!-- 参考 -->
-                <div class="form__item">
-                    <label for="reference" class="form__label">参考</label>
-                    <input type="text" name="reference" class="form__input" id="reference" value="">
-                    <span class="err-msg"></span>
-                </div>
-                <!-- その他 -->
-                <div class="form__item">
-                    <label for="etc" class="form__label">その他</label>
-                    <textarea name="etc" id="etc" class="form__textarea"></textarea>
-                    <span class="err-msg"></span>
-                </div>
-                <div class="form__footer">
-                    <div class="btn-container">
+
+                <div class="memo-form__footer">
+                    <div class="btn-wrap">
                         <input type="submit" name="<?= ($editFlag) ? 'edit_memo' : 'create_memo'; ?>" value="<?= ($editFlag) ? '編集' : '保存'; ?>" class="submit-btn">
                     </div>
                 </div>
