14e5bce96eaa9f459e58627c7afbcccf69e5d534
kawada68 <nodoguro68@gmail.com>
2022-02-09 14:37:15 +0900

    管理画面スタイル作成

--- app/admin.php
+++ app/admin.php
@@ -6,19 +6,60 @@ include '../template/header.php';
 ?>
 
 <main class="main">
-    <div class="container">
+    <div class="container container--2column">
+        <aside class="sidebar">
+            <div class="sidebar__inner">
+                <div class="admin-info">
+                    <h2 class="admin-info__title">管理画面</h2>
+                    <p class="admin-info__name">@ユーザー</p>
+                </div>
+                <ul class="link-list">
+                    <li class="link-list__item"><a href="admin.php" class="link-list__link">HOME</a></li>
+                    <li class="link-list__item"><a href="category.php" class="link-list__link">カテゴリー</a></li>
+                </ul>
+            </div>
+        </aside>
 
-        <section class="section">
-            <h2 class="title">管理画面</h2>
+        <section class="contents">
+            <div class="contents__inner">
+                <section class="admin-contents">
+                    <h2 class="admin-contents__title">カテゴリー一覧</h2>
 
-            <div class="link-wrap">
-                <a href="category.php" class="link">カテゴリー</a>
+                    <div class="err-msg__area">
+                        <span class="err-msg">エラー</span>
+                    </div>
+
+                    <form method="post" class="category-form">
+                        <input type="text" name="category" class="category-form__input" placeholder="カテゴリーを入力">
+                        <input type="submit" value="保存" class="category-form__submit-btn">
+                    </form>
+
+                    <ul class="category-list">
+                        <li class="category-list__item">
+                            <span class="category-list__name">カテゴリー</span>
+                            <button type="button" class="category-list__btn">削除</button>
+                        </li>
+                    </ul>
+                </section>
             </div>
         </section>
-
     </div>
 
+    <div class="modal">
+        <div class="modal__inner">
+            <p class="modal__text">本当に削除してよろしいですか？</p>
+            <form method="post" class="modal__form">
+                <div class="btn-wrap">
+                    <button type="button" class="modal-btn modal-btn--cancel">キャンセル</button>
+                    <input type="submit" name="delete_category" value="削除" class="modal-btn modal-btn--delete">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="modal-bg"></div>
 </main>
 <?php include '../template/footer.php' ?>
 </body>
+
 </html>
--- app/category.php
+++ app/category.php
@@ -1,35 +1,17 @@
-<?php
+<section class="section">
+    <h2 class="title">カテゴリー</h2>
 
-$page_title = 'カテゴリー';
-include '../template/head.php';
-include '../template/header.php';
-?>
+    <form method="post" class="category-form">
+        <input type="text" name="category" class="form__input">
+        <input type="submit" value="保存" class="submit-btn">
+    </form>
 
-<main class="main">
-    <div class="container">
-
-        <section class="section">
-            <h2 class="title">カテゴリー</h2>
-
+    <ul class="category-list">
+        <li class="category-list__item">
+            <span class="category-list__name">カテゴリー</span>
             <form method="post" class="category-form">
-                <input type="text" name="category" class="form__input">
-                <input type="submit" value="保存" class="submit-btn">
+                <button class="delete-btn">削除</button>
             </form>
-
-            <ul class="category-list">
-                <li class="category-list__item">
-                    <span class="category-list__name">カテゴリー</span>
-                    <form method="post" class="category-form">
-                        <button class="delete-btn">削除</button>
-                    </form>
-                </li>
-            </ul>
-        </section>
-
-    </div>
-
-</main>
-<?php include '../template/footer.php' ?>
-</body>
-
-</html>
+        </li>
+    </ul>
+</section>
