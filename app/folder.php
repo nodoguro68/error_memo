47c92a615af706b6d501ef01a4d1a435cfa73441
kawada68 <nodoguro68@gmail.com>
2022-02-10 16:01:22 +0900

    フォルダのスタイル作成

--- app/folder.php
+++ app/folder.php
@@ -7,19 +7,30 @@ include '../template/header.php';
 
 <main class="main">
     <div class="container">
-        フォルダタイトル
-        <section class="section">
-            <div class="section__header">
-                <h2 class="section__title">フォルダ</h2>
-                <button class="section__delete-btn">削除</button>
+        <section class="folder">
+            <div class="folder__header">
+                <h2 class="folder__title">フォルダ</h2>
+                <button type="button" class="folder__delete-btn">削除</button>
             </div>
-            <div class="section__body">
+            <div class="folder__body">
                 <ul class="memo-list">
                     <ul class="memo-list">
                         <li class="memo-list__item">
-                            <a href="memo_form.php" class="memo-list__link">
-                                メモ
-                                <span class="memo-list__date">2022/01/01</span>
+                            <a href="memo_form.php?memo_id=" class="memo-list__link">
+                                <div class="memo-list__head">
+                                    <span class="memo-list__label memo-list__label--unsolved">未解決</span>
+                                    <span class="memo-list__date">2022/01/01</span>
+                                </div>
+                                <span class="memo-list__title">未解決メモ</span>
+                            </a>
+                        </li>
+                        <li class="memo-list__item">
+                            <a href="memo_form.php?memo_id=" class="memo-list__link">
+                                <div class="memo-list__head">
+                                    <span class="memo-list__label memo-list__label--solved">解決済</span>
+                                    <span class="memo-list__date">2022/01/01</span>
+                                </div>
+                                <span class="memo-list__title">未解決メモ</span>
                             </a>
                         </li>
                     </ul>
@@ -28,6 +39,22 @@ include '../template/header.php';
         </section>
 
     </div>
+
+    <div class="modal">
+        <div class="modal__head">
+            <p class="modal__text">フォルダ内の全てのメモも削除されますがよろしいですか？</p>
+        </div>
+        <div class="modal__body">
+            <form method="post" class="modal__form">
+                <div class="btn-wrap">
+                    <button type="button" class="modal-btn modal-btn--cancel">キャンセル</button>
+                    <input type="submit" name="delete_folder" value="削除" class="modal-btn modal-btn--delete">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="modal-bg"></div>
 
 </main>
 <?php include '../template/footer.php' ?>
