d324e575b29f39acd61433014412a6a41a48edab
kawada68 <nodoguro68@gmail.com>
2022-02-17 16:33:54 +0900

    削除ボタンをアイコンに変更
    メモリストを修正

--- app/folder.php
+++ app/folder.php
@@ -10,28 +10,25 @@ include '../template/header.php';
         <div class="contents">
             <div class="contents__head">
                 <h2 class="contents__title">フォルダ</h2>
-                <button type="button" class="contents__delete-btn" id="openModalBtn">削除</button>
+                <button type="button" class="contents__delete-btn" id="openModalBtn"><i class="fas fa-trash-alt"></i></button>
             </div>
             <div class="contents__body">
                 <ul class="memo-list">
-                    <ul class="memo-list">
-                        <li class="memo-list__item">
-                            <a href="memo_form.php?memo_id=" class="memo-list__link">
-                                <div class="memo-list__head">
-                                    <span class="memo-list__label memo-list__label--unsolved">未解決</span>
-                                    <span class="memo-list__category">カテゴリー</span>
-                                    <span class="memo-list__date">2022/01/01</span>
-                                </div>
-                                <div class="memo-list__body">
-                                    <span class="memo-list__title">未解決メモ</span>
-                                </div>
-                                <div class="memo-list__foot">
+                    <li class="memo-list__item">
+                        <a href="memo_form.php?memo_id=" class="memo-list__link">
+                            <div class="memo-list__head">
+                                <span class="memo-list__label memo-list__label--unsolved">未解決</span>
+                                <span class="memo-list__category">カテゴリー</span>
+                                <span class="memo-list__date">2022/01/01</span>
+                            </div>
+                            <div class="memo-list__body">
+                                <span class="memo-list__title">未解決メモ</span>
+                            </div>
+                            <div class="memo-list__foot">
 
-                                </div>
-                            </a>
-                        </li>
-
-                    </ul>
+                            </div>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
