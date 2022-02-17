76925cdc34de6c4181bb41ca50209a89df18c598
kawada68 <nodoguro68@gmail.com>
2022-02-17 16:32:56 +0900

    お気に入りメモ一覧を修正

--- app/mypage.php
+++ app/mypage.php
@@ -75,21 +75,6 @@ include '../template/header.php';
                         </div>
                     </a>
                 </li>
-                <li class="memo-list__item">
-                    <a href="" class="memo-list__link">
-                        <div class="memo-list__head">
-                            <span class="memo-list__label memo-list__label--private">非公開</span>
-                            <span class="memo-list__category">カテゴリー</span>
-                            <span class="memo-list__date">2022/01/01</span>
-                        </div>
-                        <div class="memo-list__body">
-                            <span class="memo-list__title">解決済メモ</span>
-                        </div>
-                        <div class="memo-list__foot">
-                            <span class="memo-list__folder"><i class="far fa-folder"></i>フォルダ</span>
-                        </div>
-                    </a>
-                </li>
             </ul>
             </section>
         </div>
@@ -107,7 +92,7 @@ include '../template/header.php';
                         </div>
                         <div class="memo-list__foot">
                             <div class="memo-list__user-info">
-                                <img src="../public/images/no-image.jpeg" alt="" class="memo-list__profile-img">
+                                <img src="../public/images/no-image.jpeg" alt="プロフィール画像" class="memo-list__profile-img">
                                 <span class="memo-list__username">@ユーザー</span>
                             </div>
                             <div class="memo-list__favor">
@@ -127,8 +112,8 @@ include '../template/header.php';
                         <div class="img-wrap">
                             <img src="../public/images/no-image.jpeg" alt="プロフィール画像" class="user-info__profile-img">
                         </div>
-                        <p class="user-info__name">@ユーザーネーム</p>
-                        <p>登録日:<span class="user-info__regisration-date">2022/01/01</span></p>
+                        <span class="user-info__name">@ユーザーネーム</span>
+                        <div class="user-info__regisration-date">登録日:2022/01/01</div>
                         <div class="user-info__count-area">
                             <div class="user-info__count-item">
                                 未解決<span class="user-info__count">50</span>件
